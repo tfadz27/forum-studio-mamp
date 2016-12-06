94d8a7782a443fb405b74b206ef9ec4c2ff0e126
Terry Fadz <terry.fadz@thirdwavellc.com>
2016-12-06 14:45:20 -0600

    Updates to People page and single people page

--- wp-content/themes/forum-studio/page-leadership.php
+++ wp-content/themes/forum-studio/page-leadership.php
@@ -9,26 +9,45 @@
  
       <section class="fs-container">
         <div class="fs-container__col-9">
+        <h1 class="fs-h1">Leadership</h1>
+        <h4 class="fs-h4"><?php the_field('leadership_title'); ?></h4>
+        
+        <div style="width: 100%;max-width: 700px;"><?php the_field('leadership_content'); ?></div>
+        </div>
+
+         <aside class="fs-container__col-3 fs-leadership-sidebar">
+          <h5 class="fs-h5">LEADERSHIP BY INDUSTRY</h5>
+          <ul>
+          <li><a href="#">Corporate</a></li>
+          <li><a href="#">Industrial</a></li>
+          <li><a href="#">Institutional</a></li>
+          <li><a href="#">Residential</a></li>
+          </ul>
+        </aside>
           
           <section class="fs-leaders">
-           <?php $recentPosts = new WP_Query(array('posts_per_page' => -1, 'post_type' => array('leadership') ));
+            <?php $recentPosts = new WP_Query(array('posts_per_page' => -1, 'post_type' => array('leadership') ));
             while( $recentPosts->have_posts() ) :  $recentPosts->the_post();  ?>
                 
                 <article class="fs-leadership-person">
                   <div class="thumb"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('image') ?></a></div>
-                   <div class="fs-leadership-card">
-                    <span><a href="<?php the_permalink(); ?>" class="name"><?php the_title(); ?></a>, <span class="title"><?php the_field('title'); ?></span></span>
                    
-                    <span class="email"><a href="mailto: <?php the_field('email'); ?>"><?php the_field('email'); ?></a></span>
+                   <div class="fs-leadership-card">
+                    <span class="name-title"><a href="<?php the_permalink(); ?>" class="name"><?php the_title(); ?></a>, 
+                    <span class="title"><?php the_field('title'); ?></span></span>
+                   
                     <span class="phone"><?php the_field('phone'); ?></span>
+                    <span class="email"><a href="mailto: <?php the_field('email'); ?>"><?php the_field('email'); ?></a></span>
+                    <span class="fs-link"><a href="<?php the_permalink(); ?>">Read More</a></span>
+
                   </div>
                 </article>
                 <?php endwhile;?>
                
-          </div>
+          
 
         </div>
-        <aside class="fs-container__col-3"></aside>
+
 
       </section>
   </main><!-- .site-main -->
---
+++ wp-content/themes/forum-studio/single-leadership.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Forum_Studio
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<section class="fs-container">
+        <div class="fs-container__col-3">
+        	<div class="thumb"><?php the_post_thumbnail('image') ?></a></div>
+
+					<div class="fs-leadership__single-card">
+						<h1 class="fs-h1">Contact</h1>
+		      		<span class="phone"><?php the_field('phone'); ?></span>
+		      		<a href="mailto: <?php the_field('email'); ?>"><?php the_field('email'); ?></a>
+		      </div>
+
+					<nav class="fs-leadership__nav">
+		      	<span class="fs-leadership__nav--prev"><?php previous_post_link('&laquo; &laquo; %',
+ 'Toward The Past: ', 'no'); ?></span>
+		      	<span class="fs-leadership__nav--next"><?php next_post_link('&laquo; &laquo; %',
+ 'Toward The Future: ', 'no'); ?></span>
+		      </nav>
+        </div>
+        
+
+        <div class="fs-container__col-7">
+				 <h4 class="fs-h4"><?php the_title(); ?>, 
+         <span class="title"><?php the_field('title'); ?></span></h4>
+         <span class="bio"><?php the_field('bio'); ?></span>
+
+
+					<?php
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'template-parts/content', get_post_format() );
+
+						the_post_navigation();
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+
+					endwhile; // End of the loop.
+					?>  
+			
+
+      </div><!-- col 7 -->
+      <div class="fs-container__col-3">
+      	<h5 class="fs-h5">RELATED CONTENT</h5>
+      </div>
+         
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
